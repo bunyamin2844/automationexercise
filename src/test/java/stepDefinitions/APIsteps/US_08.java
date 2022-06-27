71c2e210f1307dcda69e369e48d5ccb3820e5495
Bunyamin <bunyaminbektas2844@gmail.com>
2022-06-27 20:03:20 +0100

    son

--- src/test/java/stepDefinitions/APIsteps/US_08.java
+++ src/test/java/stepDefinitions/APIsteps/US_08.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import pojo.Login;
 
@@ -17,26 +18,31 @@ public class US_08 {
     Login login;
 
        String url ="https://automationexercise.com/api/verifyLogin";
-       String url2="https://automationexercise.com/api/getUserDetailByEmail";
+
        @Test
     public void test008() throws IOException {
 
            Response response= given()
                    .contentType(ContentType.JSON)
-                  // .post(url);
-                    .post(url2);
+                   .post(url);
+
            //response.prettyPrint();
 
            JsonPath jsonPath= response.jsonPath();
-           System.out.println("jsonPat) = " + jsonPath.getInt("responseCode"));
-           System.out.println("jsonP\") = " + jsonPath.getString("message"));
+           System.out.println("Response code " + jsonPath.getInt("responseCode"));
+           System.out.println("Message = " + jsonPath.getString("message"));
+         //  Assert.assertEquals(jsonPath.getString("message"),equals(400));
 
-           //*********************************
+           System.out.println("*********************************");
            
            Login login = objectMapper.readValue(response.asString(),Login.class);
 
            System.out.println("login.getResponseCode() = " + login.getResponseCode());
            System.out.println("login.getMessage() = " + login.getMessage());
+
+          Integer resp =400;
+           Assert.assertEquals(resp,login.getResponseCode());
+
 
 
        }
